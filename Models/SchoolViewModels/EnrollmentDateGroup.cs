9aaa10f572bdf05cc805f9c98faf5b858abb1cb0
madrara256 <herbertichama@outlook.com>
2021-10-03 21:15:34 +0300

    Create the view model

---
+++ Models/SchoolViewModels/EnrollmentDateGroup.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class EnrollmentDateGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime ? EnrrollmentDate { get; set; }
+
+        public int StudentCount { get; set; }
+    }
+}
